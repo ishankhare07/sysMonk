115f09a2b26145af61997b14883d66895767fcc3
Ishan Khare <ishankhare07@gmail.com>
2016-03-02 11:34:38 +0530

    removed unnecessary ipcMain calls

--- main.js
+++ main.js
@@ -1,32 +1,22 @@
-'use strict';
-
 const electron      = require('electron');
 const app           = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const ipcMain = require('electron').ipcMain;
+const ipcMain       = require('electron').ipcMain;
 
 var mainWindow = null;
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow({width: 300,
-         height: 735,
+         height: 740,
          frame: false,
          transparent: true,
          resizable: false
-     });
+    });
     mainWindow.loadURL('file://' + __dirname + '/index.html');
-    mainWindow.setPosition(1060, 20);
+    mainWindow.setPosition(1060, 0);
+    mainWindow.setVisibleOnAllWorkspaces(true);
 
     mainWindow.on('closed', function() {
         mainWindow = null;
-    })
-});
-
-
-ipcMain.on('asynchronous-message', function(event, arg) {
-    event.sender.send('asynchronous-reply', {
-        'type': 'cpuUsage',
-        'cpuId': arg.cpuId,
-        'value': 50
-    })
+    });
 });
